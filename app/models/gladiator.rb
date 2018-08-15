16d756fbcca91286435f588d9e074174b03a2f02
Vivien B <vivienbihl@gmail.com>
2018-08-15 18:41:17 +0200

    ajout logique pour les combats

--- app/models/gladiator.rb
+++ app/models/gladiator.rb
@@ -14,6 +14,55 @@ class Gladiator < ApplicationRecord
     name
   end
 
+  def hit_chance
+    rand(1..6) + rand(1..6) + self.agility
+  end
+
+  def damage
+    rand(2..self.strength)
+  end
+
+  def dead?
+    @current_hp <= 0
+  end
+
+  def fight opponent
+    if self == opponent
+      raise
+    end
+
+    @current_hp = max_hp
+    opponent.current_hp = opponent.max_hp
+
+    puts @current_hp
+    puts opponent.current_hp
+
+    turns = []
+
+    while !dead? and !opponent.dead?
+      home_hit_chance = hit_chance
+      away_hit_chance = opponent.hit_chance
+
+      if home_hit_chance == away_hit_chance
+        turns.append [:miss, nil, nil, nil]
+      elsif home_hit_chance > away_hit_chance
+        dmg = damage
+        opponent.current_hp -= dmg
+        turns.append [:hit, self.to_s, opponent.to_s, dmg]
+      else
+        dmg = opponent.damage
+        @current_hp -= dmg
+        turns.append [:hit, opponent.to_s, self.to_s, dmg]
+      end
+    end
+
+    winner = dead? ? opponent : self
+
+    Fight.create home: self, away: opponent, winner: winner
+
+    turns
+  end
+
   private
 
   def gen_stat
@@ -25,6 +74,7 @@ class Gladiator < ApplicationRecord
     self.strength = gen_stat
     self.agility = gen_stat
 
-    self.max_hp = self.stamina + rand(1..6) + rand(1..6)
+    self.max_hp = (self.stamina + rand(1..6) + rand(1..6)) * 10
   end
+
 end
