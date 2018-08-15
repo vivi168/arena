dcdb4192501e2b52c21513f845724c2b2cc0f153
Vivien B <vivienbihl@gmail.com>
2018-08-15 18:40:21 +0200

    pouvoir creer un combat sans vainqueur au debut

--- app/models/fight.rb
+++ app/models/fight.rb
@@ -1,7 +1,7 @@
 class Fight < ApplicationRecord
   belongs_to :home, class_name: "Gladiator"
   belongs_to :away, class_name: "Gladiator"
-  belongs_to :winner, class_name: "Gladiator"
+  belongs_to :winner, class_name: "Gladiator", required: false
 
   validate :not_fighting_self
   validate :winner_must_participate
@@ -13,6 +13,6 @@ class Fight < ApplicationRecord
   end
 
   def winner_must_participate
-    errors.add(:away, I18n.t('fight.errors.winner_must_participate')) if ![home,away].include?(winner)
+    errors.add(:away, I18n.t('fight.errors.winner_must_participate')) if winner.present? and ![home,away].include?(winner)
   end
 end
